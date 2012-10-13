1ebc6171eb3c38de2777fd92ddf26738fe3d6ab0
Merca <merca666@gmail.com>
2012-10-13 09:46:41 +0200

    Commit build files

---
+++ build/generated-sources/jax-ws/car/services/jaxws/GetAllReservation.java
@@ -0,0 +1,15 @@
+
+package car.services.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "getAllReservation", namespace = "http://services.car/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getAllReservation", namespace = "http://services.car/")
+public class GetAllReservation {
+
+
+}
